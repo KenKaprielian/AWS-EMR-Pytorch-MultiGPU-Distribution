f9354ad28068bd6dde64220a40ea22accb21f271
KenKaprielian <30817280+KenKaprielian@users.noreply.github.com>
2020-09-27 06:00:23 -0400

    missing comma

--- pytorch.py
+++ pytorch.py
@@ -44,7 +44,7 @@ dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
               for x in ['train', 'val']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 class_names = image_datasets['train'].classes
-print('class_names:'class_names)
+print('class_names:',class_names)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print('device:',device)
 def train_model(model, criterion, optimizer, scheduler, num_epochs=3):

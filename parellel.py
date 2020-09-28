9c4d4d3a948cfdc368b7e0aefecf410a7befd58d
KenKaprielian <30817280+KenKaprielian@users.noreply.github.com>
2020-09-28 01:42:21 -0400

    uncomment multi gpu logic

--- parellel.py
+++ parellel.py
@@ -42,10 +42,10 @@ class Model(nn.Module):
         return output
 
 model = Model(input_size, output_size)
-#if torch.cuda.device_count() > 1:
-  #print("Let's use", torch.cuda.device_count(), "GPUs!")
+if torch.cuda.device_count() > 1:
+  print("Let's use", torch.cuda.device_count(), "GPUs!")
   # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-  #model = nn.DataParallel(model)
+  model = nn.DataParallel(model)
 
 model.to(device)
 
